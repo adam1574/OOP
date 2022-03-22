b997b6536bdfae89691650ba960d972c3aeebf95
Sivera.Adam <sivera.adam@seznam.cz>
2022-03-22 11:35:20 +0100

    sss

--- OOP/MainWindow.xaml.cs
+++ OOP/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OOP
 {
@@ -23,9 +24,13 @@ namespace OOP
 
         car auto1;
         car auto2;
-
+        DispatcherTimer timer;
         public MainWindow()
         {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 2);
+            timer.Tick += Timer_Tick;
+
             InitializeComponent();
 
 
@@ -49,23 +54,51 @@ namespace OOP
             textBox.Text += $"Fuel level: {car.FuelLevel}\n";
             textBox.Text += $"Distance: {car.Distance}\n";
             textBox.Text += $"Pneu stav: {Math.Round(car.PneuState, 2)}\n";
+            textBox.Text += $"Speedometr: {car.Speedometer}km/h\n";
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
             auto1.Move();
             auto2.Move();
             ShowVehicle(auto1, txt1);
             ShowVehicle(auto2, txt2);
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            auto1.Move();
+            auto2.Move();
+        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             auto1.FuelLevel = 100;
-            ShowVehicle(auto1, txt1);
             auto2.FuelLevel = 100;
+            ShowVehicle(auto1, txt1);
             ShowVehicle(auto2, txt2);
 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            auto1.PneuState = 100;
+            auto2.PneuState = 100;
+            ShowVehicle(auto1, txt1);
+            ShowVehicle(auto2, txt2);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            auto1.SpeedometrUp();
+            ShowVehicle(auto1, txt1);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            auto1.SpeedometrDown();
+            ShowVehicle(auto1, txt1);
+            ShowVehicle(auto2, txt2);
         }
     }
 }
--- OOP/car.cs
+++ OOP/car.cs
@@ -13,6 +13,7 @@ namespace OOP
         public int FuelLevel { get; set; }
         public int Distance { get; set; }
         public double PneuState { get; set; }
+        public int Speedometer { get; set; }
 
         public car(string brand)
         {
@@ -23,13 +24,36 @@ namespace OOP
             FuelLevel = 100;
             Distance = 0;
             PneuState = 100;
+            Speedometer = 0;
+
         }
         public void Move()
         {
             FuelLevel -= 1;
             Distance += 10;
             PneuState -= 0.01;
+            if (Speedometer > 0)
+            {
+                FuelLevel = Math.Max(0, FuelLevel - 1);
 
+                // FuelLevel = Math.Min(100, FuelLevel + 50);
+
+                if (FuelLevel > 0)
+                {
+                    Distance += Speedometer;
+                    PneuState -= Speedometer / 100D;
+                }
+            }
+        }
+
+        public void SpeedUp()
+        {
+            Speedometer = Math.Min(100, Speedometer + 1);
+        }
+
+        public void SpeedDown()
+        {
+            Speedometer = Math.Max(0, Speedometer - 1);
         }
     }
 
